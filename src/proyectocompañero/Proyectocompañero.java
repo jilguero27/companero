017a6731229238b6ab753b1e9aedeebbbf4abe7a
dam <dam@203P01>
2018-02-07 13:38:15 +0100

    segunda version compañero

--- src/proyectocompañero/Proyectocompañero.java
+++ src/proyectocompañero/Proyectocompañero.java
@@ -15,7 +15,9 @@ public class Proyectocompañero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola compañero");  
         System.out.println("Hola compañero");
+        
     }
     
 }
